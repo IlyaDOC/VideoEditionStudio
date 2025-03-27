3972a045f3b257e507dd03ff1e45b217a0a43c33
IlyaDOC <ilya22@inbox.ru>
2025-03-27 16:08:40 +0300

    last commit 27/03/25

--- src/scripts/includes/slider.js
+++ src/scripts/includes/slider.js
@@ -2,5 +2,57 @@ import Swiper from 'swiper';
 import { Navigation, Pagination, Autoplay, FreeMode, Thumbs, Scrollbar } from 'swiper/modules';
 
 export function initializeSlider() {
-
+    const companiesSlider = new Swiper(".companies-section .swiper", {
+        modules: [Autoplay, FreeMode],
+        slidesPerView: 3.15,
+        spaceBetween: 12,
+        speed: 3000,
+        loop: true,
+        freeMode: {
+            enabled: true,
+            sticky: false
+        },
+        autoplay: {
+            delay: 0,
+            pauseOnMouseEnter: true,
+        },
+        breakpoints: {
+            744: {
+                slidesPerView: 6.4,
+                speed: 2000,
+            },
+            1200: {
+                slidesPerView: 5.7,
+                spaceBetween: 4
+            }
+        }
+    });
+    const reviewsSlider = new Swiper(".reviews-section .swiper", {
+        modules: [Navigation],
+        slidesPerView: 1,
+        spaceBetween: 12,
+        navigation: {
+            prevEl: ".reviews-section .navigation-button--prev",
+            nextEl: ".reviews-section .navigation-button--next",
+        },
+        breakpoints: {
+            744: {
+                slidesPerView: 2.7,
+                spaceBetween: 40
+            },
+            1200: {
+                slidesPerView: 4.3,
+                spaceBetween: 40
+            },
+            1400: {
+                slidesPerView: 5.2,
+                spaceBetween: 40
+            },
+            1920: {
+                slidesPerView: 5.4,
+                speed: 4000,
+                spaceBetween: 80
+            }
+        }
+    })
 }
---
+++ src/scripts/shared/features/input-error.js
@@ -0,0 +1,15 @@
+export default function inputError() {
+    const requiredInputs = document.querySelectorAll("input[required]");
+
+    if (requiredInputs) {
+        requiredInputs.forEach(requiredInput => {
+            requiredInput.addEventListener("invalid", function () {
+                this.classList.add("error");
+            });
+
+            requiredInput.addEventListener("valid", function () {
+                this.classList.remove("error");
+            });
+        })
+    }
+}
